2e0bdcf81ca88b7cad6cef9cdf7d33717e50d012
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-12 16:49:52 +0100

    i moved background image to App to wrap entire app and set it to fixed

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -2,17 +2,23 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navigation from "../components/Navigation/navigation";
 import BaseSection from "../components/BaseSection/baseSection";
+import Cat from '../components/BaseSection/images/white-4557097_1920.jpg'
 
 class  App extends Component {
     render() {
+        const style = {
+            backgroundImage: `url('${Cat}')`
+        };
         return (
             <>
                 <Router>
+                    <div className={'background'} style={style}>
                     <Navigation/>
                     <BaseSection/>
                     <section className={'dupa'}>
                         <div>dupa</div>
                     </section>
+                    </div>
                 </Router>
             </>
         )
