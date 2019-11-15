9056fd020c0739d97bb1558cc1910dc70c2792c8
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-16 00:38:14 +0100

    FSLightbox set for breedsgallery

--- src/js/components/Main Components/Breeds/breeds.js
+++ src/js/components/Main Components/Breeds/breeds.js
@@ -2,11 +2,13 @@ import React,{useState, useEffect} from 'react';
 import axios from 'axios';
 import Logo from "./Subcomponents/Logo/logo";
 import Select from "./Subcomponents/Select/select";
+import FSLightBox from "fslightbox-react";
 
 function Breeds() {
     const [breeds, fetchBreeds] = useState([]);
     const [breed, setBreed] = useState('abys');
     const [gallery, loadGallery] = useState([]);
+    const [toggler, setToggler] = useState(false);
 
     useEffect(() => {
          const fetchData = async () => {
@@ -29,7 +31,13 @@ function Breeds() {
     },[breed]);
     console.log(breed);
     console.log(gallery);
+
+    const arrayUrls = gallery.map((el) => el.url);
+    console.log(arrayUrls);
+
+
     return (
+        <>
         <div className={'wrapper'}>
             <div className={'breedsWrapper'}>
                 <Logo/>
@@ -37,8 +45,9 @@ function Breeds() {
                 <div className={'gallery'}>
                     {gallery
                         .slice(0,3)
-                        .map(el =>
-                        <div key={el.id} className={'breedGallery-wrap'}>
+                        .map((el) =>
+                        <div key={el.id} className={'breedGallery-wrap'}
+                                            onClick={() => setToggler(!toggler)}>
                             <img
                             className={'breedGallery-image'}
                             src={el.url}
@@ -47,6 +56,14 @@ function Breeds() {
                 </div>
             </div>
         </div>
+            <FSLightBox
+                toggler={toggler}
+                sources={arrayUrls}
+                showThumbsOnMount={ true }
+                key={arrayUrls}
+                type={'image'}
+            />
+            </>
     )
 }
 
