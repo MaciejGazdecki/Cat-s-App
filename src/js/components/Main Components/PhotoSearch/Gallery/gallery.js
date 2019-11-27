b1769e62ab15bbd0c9c8098f48d3a133a0c25a55
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-27 19:29:19 +0100

    problems with pagination, request help

--- src/js/components/Main Components/PhotoSearch/Gallery/gallery.js
+++ src/js/components/Main Components/PhotoSearch/Gallery/gallery.js
@@ -5,8 +5,10 @@ import PropTypes from 'prop-types';
 function Gallery(props) {
     const {category,type,breedID} = props;
     const [gallery, setGallery] = useState([]);
+    let page = 1;
+    const perPage = 5;
 
-    const params = {
+    let params = {
         mime_types: type,
         category_ids: category,
         breed_ids: breedID,
@@ -15,6 +17,7 @@ function Gallery(props) {
         order: 'DESC'
 
     };
+
     useEffect(() =>{
         const fetchData = async () => {
             return await axios.get(`/images/search`, {params})
@@ -23,15 +26,34 @@ function Gallery(props) {
             .then(res => {console.log(res.data); setGallery(res.data)})
             .catch(err => console.log(err))
     }, [breedID,category,type]);
-    return (
-        <div className="galleryWrapper">
-            <div className='photos'>
-                {gallery.map(el =>
+
+    let images =
+        <>
+            {gallery.slice(page*perPage -perPage, page*perPage).map(el =>
                 <div key={el.id}
                      style={{backgroundImage: `url(${el.url})`}}
                      className='galleryImage'>
-                </div>)}
+                </div>)};
+        </>;
+
+    function onClickNextHandler() {
+        page++;
+    }
+
+    function onClickPreviousHandler() {
+        if(page> 1) page--;
+    }
+    console.log(gallery);
+    console.log(page);
+    //hej Przemek, dlaczego mi nie zwiekszaja lub zmiejszaja handlery page :)?? probowalem w onClick callbacka dac i to samo :(,
+    //nie moge zrobic pagination :(
+    return (
+        <div className="galleryWrapper">
+            <div className='photos'>
+                {images}
             </div>
+            <button onClick={onClickPreviousHandler}>Previous page</button>
+            <button onClick={onClickNextHandler}>Next page</button>
         </div>
     )
 
