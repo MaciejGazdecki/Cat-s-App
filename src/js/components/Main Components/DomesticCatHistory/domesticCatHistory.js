babc377aa2e458f77e64859d4e864f0bad77b952
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-27 15:39:07 +0100

    small bug fix

--- src/js/components/Main Components/DomesticCatHistory/domesticCatHistory.js
+++ src/js/components/Main Components/DomesticCatHistory/domesticCatHistory.js
@@ -15,7 +15,7 @@ function DomesticCatHistory() {
                         is a small carnivorous mammal It is the only domesticated species in the
                         family Felidae and often referred to as the domestic cat to distinguish it from wild members of
                         the family. The cat is either a house cat or a farm cat, which are pets, or a feral cat, which
-                        ranges freely and avoids human contact.A house cat is valued by humans for companionship and
+                        ranges freely and avoids human contact. A house cat is valued by humans for companionship and
                         for its ability to hunt rodents. About 60 cat breeds are recognized by various cat registries.
                         The cat is similar in anatomy to the other felid species, has a strong flexible body, quick
                         reflexes, sharp teeth and retractable claws adapted to killing small prey. Its night vision 
@@ -32,9 +32,10 @@ function DomesticCatHistory() {
                         as abandonment of pets, resulted in large numbers of feral cats worldwide, contributing to the
                         extinction of entire bird species, and evoking population control.
                         It was long thought that cat domestication was initiated in Egypt, because cats in ancient
-                        Egypt were venerated since around 3100 BC.However, the earliest indication for the taming 
-                        of an African wildcat (F. lybica) was found in Cyprus, where a cat skeleton was excavated
-                        close by a human Neolithic grave dating to around 7500 BC. African wildcats were probably
+                        Egypt were venerated since around 3100 BC. However, the earliest indication for the taming 
+                        of an African wildcat <span className='italic'>(F. lybica) </span>was found in Cyprus,
+                        where a cat skeleton was excavated close by a human Neolithic grave dating to around 7500 BC. 
+                        African wildcats were probably
                         first domesticated in the Near East. As of 2017, the domestic cat was the second-most popular
                         pet in the United States by number of pets owned, after freshwater fish, with 95 million cats
                         owned. In the United Kingdom, around 7.3 million cats lived in more than 4.8 million
