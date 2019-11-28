0a988c752d776d06dff12fd8c918f1ef8707c9fa
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-28 02:49:47 +0100

    I moved pagination to another component to clear te code

--- src/js/components/Main Components/PhotoSearch/Gallery/gallery.js
+++ src/js/components/Main Components/PhotoSearch/Gallery/gallery.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import PropTypes from 'prop-types';
+import Pagination from "./Pagination/pagination";
 
 function Gallery(props) {
     const {category,type,breedID} = props;
@@ -8,6 +9,7 @@ function Gallery(props) {
     const [page, setPage] = useState(1);
     const [paramsPage, setParamsPage] = useState(1);
     const perPage = 5;
+
     let params = {
         mime_types: type,
         category_ids: category,
@@ -32,23 +34,6 @@ function Gallery(props) {
         downloadData();
     }, [breedID,category,type]);
 
-    const  onClickNextHandler = () => {
-        if (gallery.slice(page*perPage - perPage, page*perPage).length < perPage || page === params.limit / perPage) {
-            setPage(prevState => prevState);
-        } else {
-            setPage(prevState => prevState +1)
-        }
-    };
-
-    const onClickPreviousHandler = () => {
-        if(page> 1) setPage(prevState => prevState -1);
-    };
-
-    const onClickLoadMoreHandler = () => {
-      setParamsPage(prevState => prevState+1);
-      downloadData();
-    };
-
     const images =
         <>
             {gallery.slice(page*perPage - perPage, page*perPage).map(el =>
@@ -56,21 +41,26 @@ function Gallery(props) {
                          style={{backgroundImage: `url(${el.url})`}}
                          className='galleryImage'>
                     </div>)}
-            <div className="galleryButtons">
-                <button onClick={onClickPreviousHandler}>Previous page</button>
-                <button onClick={onClickNextHandler}>Next page</button>
-            </div>
         </>;
+
     const noMatchesFound = <div className='noMatches'>No matches found</div>;
+
     return (
         <div className="galleryWrapper">
             <div className='photos'>
                 {gallery.length > 0 ? images : noMatchesFound}
-                {page === params.limit / perPage ? <button onClick={onClickLoadMoreHandler}>Try to load more photos</button> : null}
             </div>
+            <Pagination
+                gallery={gallery}
+                page={page}
+                setPage={setPage}
+                perPage={perPage}
+                downloadData={downloadData}
+                setParamsPage={setParamsPage}
+                params={params}
+            />
         </div>
     )
-
 }
 
 export default Gallery;
