e0bc6173a90624e0302727c40181300ee391173d
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-28 05:52:27 +0100

    I added favourites section and fetched favourites photos,i added option to unlike photos

--- src/js/components/Main Components/Favourites/favourites.js
+++ src/js/components/Main Components/Favourites/favourites.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+
+const axiosInstanceHandleFavourites = axios.create({
+    baseURL: 'https://api.thecatapi.com/v1',
+    headers : {
+        'x-api-key': '687fe573-b392-44ea-b985-63c162d0f64c'
+    }
+});
 
 function Favourites() {
+    const [favourites, setFavourites] = useState([]);
+
+    const userName = localStorage.getItem('userName');
+
+    const downloadFavourites = () => {
+        const params = {
+            sub_id: userName
+        };
+        const fetchData = async () => {
+            return await axiosInstanceHandleFavourites.get('favourites', {params})
+        };
+        fetchData()
+            .then(res => {console.log(res.data); setFavourites(res.data)})
+            .catch(err => console.log(err))
+    };
+    useEffect(() => {
+        downloadFavourites()
+    },[]);
+
+    const unlikePhoto = async (id) => {
+        await axiosInstanceHandleFavourites.delete(`favourites/${id}`)
+            .then(res => {console.log(res); alert('Photo unliked')})
+            .catch(err => console.log(err));
+        downloadFavourites();
+    };
+
     return (
-        <div className='wrapper'>
-            <div className='favouritesWrapper'>
-                <h2>Wyszukiwarka zdjęć</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum cupiditate eligendi, facilis harum itaque magnam minima molestiae, necessitatibus nulla, numquam perspiciatis quibusdam quo vel. A architecto, atque consectetur delectus dicta harum incidunt minima nobis numquam perferendis, praesentium repellendus. Alias, assumenda deleniti, dolor doloremque ducimus est eo quaerat vel voluptate? Amet eligendi ipsam porro possimus quasi quisquam quo, repellat saepe veniam veritatis! A assumenda dignissimos enim harum nihil quisquam, reiciendis tempora. A accusamus fuga id molestias nostrum, odio, quidem quisquam recusandae saepe sapiente ullam voluptas! A, adipisci consectetur consequatur culpa distinctio explicabo inventore iste laboriosam libero magni mollitia nulla omnis perferendis quae quam quasi quisquam quos sequi ut voluptas! Asperiores corporis cum et facere incidunt. Aliquid architecto aspernatur dicta dolore dolorum est excepturi fugit ipsam ipsum libero magnam natus numquam perferendis quae, qui quis reiciendis sed tenetur vero voluptas voluptate voluptatibus voluptatum. Aliquam animi architecto cupiditate debitis dolore eius eligendi esse ipsum itaque iusto labore magnam molestiae necessitatibus nostrum obcaecati, omnis praesentium quae quasi quibusdam quod reiciendis repudiandae sed sequi sit soluta tempore ullam? Autem cupiditate doloribus fuga hic id illo ipsum itaque,lo impedit maiores, mollitia necessitatibus nostrum odio odit officiis porro quidem quis repellat sed similique! Adipisci asperiores blanditiis eaque eos in incidunt nihil omnis recusandae rem reprehenderit, saepe unde vitae voluptatibus! Ab dicta dolores, enim eos exercitationem nam quae reiciendis sed! Dolores necessitatibus nulla odio sed totam. Adipisci animi facere nostrum ullam? Blanditiis consequatur deleniti dolor error esse ex fugiat id laboriosam quae sapiente! Asperiores aut commodi consequatur dicta dolores doloribus eaque quam recusandae repellat repudiandae saepe tempore temporibus totam unde velit vitae voluptatum! Ad animi asperiores atque culpa delectus deleniti dicta est et eum harum ipsam labore, minima minus mollitia nobis officiis quaerat quasi quia quibusdam sint totam veritatis voluptatem. Quisquam, vel voluptatibus? Iste laboriosam necessitatibus sapiente suscipit. Aliquam aspernatur at corporis excepturi explicabo fugit iusto laudantium, nobis, odio provident quidem, sequi tempora voluptate. Aperiam earum est laborum sapiente sequi? Ex illo necessitatibus nemo. Eius hic nobis ratione recusandae vel vero? Ab asperiores atque cumque dolor eaque eum exercitationem fuga, id magnam nam non officiis perferendis quod, soluta sunt voluptate voluptates! A accusamus adipisci aliquam aliquid autem, beatae consequatur dolore eos explicabo modi molestias nulla officiis pariatur perferendis porro provident qui saepe sunt, suscipit tempora. Asperod ratione repudiandae sit vitae voluptatibus. Accusantium assumenda natus qui reprehenderit sint unde? Asperiores atque commodi deleniti dicta, dolor ducimus est excepturi facilis fugiat illum in, laborum maxime mollitia nam nesciunt placeat possimus provident quisquam reiciendis reprehenderit sequi soluta veritatis. Amet consectetur, distinctio?</p>
+        <section className='favouritesSection'>
+            <div className='wrapper favouritesWrapper'>
+                <h2>Favourites Photos</h2>
+                <div className="favGallery">
+                    {favourites.map(el =>
+                    <div key={el.id}
+                         style={{backgroundImage: `url(${el.image.url})`}}
+                         className='favGalleryImage'>
+                        <button className='favBtn'
+                        onClick={() => unlikePhoto(el.id)}>
+                            <i className="fas fa-heart-broken"></i>
+                        </button>
+                    </div>)}
+                </div>
             </div>
-        </div>
+        </section>
     )
 
 }
