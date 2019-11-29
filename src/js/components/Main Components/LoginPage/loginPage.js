da42f5ff36102fe5d57dcc4af17f7d3c5c4ab12d
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-11-29 21:04:14 +0100

    styling login

--- src/js/components/Main Components/LoginPage/loginPage.js
+++ src/js/components/Main Components/LoginPage/loginPage.js
@@ -16,13 +16,13 @@ function LoginPage() {
     if (localStorage.getItem('userName')) pageUser = localStorage.getItem('userName');
 
     const form =
-        <div className={'form-wrapper'}>
+        <div className='form-wrapper'>
             <h1>SIGN IN</h1>
             <form onSubmit={handleSubmit}>
                 <label>
                     Please put your name
-                    <input type="text" id={'login'}
-                           placeholder={'Please put your name'}
+                    <input type="text" id='login'
+                           placeholder='Please put your name'
                            value={user}
                            onChange={(event => setUser(event.target.value)) }
                     />
@@ -31,17 +31,23 @@ function LoginPage() {
             </form>
         </div>;
 
+    const logout = () => {
+      localStorage.removeItem('userName');
+      localStorage.removeItem('userID')
+    };
+
     const userOnPage =
-        <div className={'userOnPage'}>
+        <div className='userOnPage'>
             <p>{`YOU ARE CURRENTLY LOGGED AS ${pageUser.toUpperCase()}`}</p>
+            <button className='logout' onClick={logout}>LOG OUT</button>
         </div>;
 
     return (
-        <div className={'wrapper'}>
-            <div className={'loginPage'}>
+        <section className='loginSection'>
+            <div className='wrapper loginPage'>
                 {pageUser ? userOnPage : form}
             </div>
-        </div>
+        </section>
     )
 
 }
